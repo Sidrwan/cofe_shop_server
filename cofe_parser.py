aa22b1a521990f4ca4b529b6c02584cc7ef3cab6
Sidrwan <50597377+Sidrwan@users.noreply.github.com>
2019-06-16 09:32:23 +0300

    Add files via upload

--- cofe_parser.py
+++ cofe_parser.py
@@ -14,11 +14,10 @@ def get_cofe_list():
     html = get_html("https://shoko.ru/menu/kofe/")
     if html:
         soup = BeautifulSoup(html, 'html.parser')
-        all_cofe_list = soup.find(class="product-list line-3-in-row").findAll(class="title", class="price",'shoko.ru' + ["/upload/resize_cache/s1/fit_70_700x700/iblock/2f2/2f2ecd5dd54284e7b5bae80f22fb7d9e.jpg",
-                                                                                                                "/upload/resize_cache/s1/fit_70_700x700/iblock/dc0/dc0f689a7bfd9d4b44ab963a7f940d2c.jpg",
-                                                                                                                "/upload/resize_cache/s1/fit_70_700x700/iblock/290/290be3ac16503b65421e3277ca46ec21.jpg",
-                                                                                                                '/upload/resize_cache/s1/fit_70_700x700/iblock/290/290be3ac16503b65421e3277ca46ec21.jpg"
-                                                                                                                ])
+        all_cofe_list = soup.find(class="product-list line-3-in-row").findAll(class="title", class="price", 'http://shoko.ru' + "/upload/resize_cache/s1/fit_70_700x700/iblock/2f2/2f2ecd5dd54284e7b5bae80f22fb7d9e.jpg",
+                                                                                                            'http://shoko.ru' + "/upload/resize_cache/s1/fit_70_700x700/iblock/dc0/dc0f689a7bfd9d4b44ab963a7f940d2c.jpg",
+                                                                                                            'http://shoko.ru' + "/upload/resize_cache/s1/fit_70_700x700/iblock/290/290be3ac16503b65421e3277ca46ec21.jpg",
+                                                                                                            'http://shoko.ru' + "/upload/resize_cache/s1/fit_70_700x700/iblock/290/290be3ac16503b65421e3277ca46ec21.jpg",)
         result_cofe_list = []
         for cofe_list in all_cofe_list:
             title = cofe.find(class="title").text
