257dc004f99b3c0eeb08079219b2a15b2afffa78
Sidrwan <50597377+Sidrwan@users.noreply.github.com>
2019-06-22 13:59:58 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
 
-from cofe_parser import get_cofe_list
+from shoko_product_parser import get_product_list
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    title = 'Меню кофе'
-    cofe_list = get_cofe_list()
-    return render_template('index.html', page_title=title, cofe_list=cofe_list)
+    title = 'Меню Fake Шоколадницы '
+    kofe_list = get_product_list("https://shoko.ru/menu/kofe/")
+    shavarma_list = get_product_list("https://shoko.ru/menu/sendvichi-i-omlety/")
+    return render_template('index.html', page_title=title, kofe_list=kofe_list, shavarma_list=shavarma_list)
 
 if __name__=="__main__":
     app.run(debug=True)
