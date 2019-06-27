9587048bfea5f879e264d3c054095d5d19a54a3f
Roman Sidorov <vf@python.ru>
2019-06-27 20:21:10 +0300

    Первый коммит

---
+++ webapp/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+from webapp.models import db, Products
+
+from webapp.shoko_product_parser import get_product_list
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    @app.route("/")
+    def index():
+        title = 'Меню Fake Шоколадницы '
+        kofe_list = get_product_list(app.config['PRODUCTS_URL_1'])
+        shavarma_list = get_product_list(app.config['PRODUCTS_URL_2'])
+        return render_template('index.html', page_title=title, kofe_list=kofe_list, shavarma_list=shavarma_list)
+
+    return app
