4ea2da63ac8773d245e74ba5469252960297e551
Roman Sidorov <vf@python.ru>
2019-07-07 21:45:02 +0300

    Парсинг описания

--- webapp/shoko_product_parser.py
+++ webapp/shoko_product_parser.py
@@ -18,20 +18,23 @@ def get_product_list(url):
     html = get_html(url)
     if html:
         soup = BeautifulSoup(html, 'html5lib')
-        all_product_list = soup.findAll('div', class_='product-card__inner')
+        all_product_list = soup.findAll('div', class_=['product-card__inner','sideInnerText'])
         result_product_list = []
         for item in all_product_list:
             image_link = 'https://shoko.ru' + item.find('img')['data-src']
             title = item.find('h4').text
-            price = item.find('div', class_='price').text.split()[0]
+            price = item.find('span').text
+            description = item.find('p').text
+            print(description)
+            
 
-            save_Products(title=title, price=price, image_link=image_link)
+            save_Products(title=title, price=price, image_link=image_link, description=description)
 
 
 
-def save_Products(title, price, image_link):
-    Products_exists = Products.query.filter(Products.image_link == url).count()
+def save_Products(title, price, image_link, description):
+    Products_exists = Products.query.filter(Products.image == image_link).count()
     if not Products_exists:
-        new_Products = Products(title=title, price=price, image_link=image_link)
+        new_Products = Products(title=title, price=price, image=image_link, description=description)
         db.session.add(new_Products)
         db.session.commit()
