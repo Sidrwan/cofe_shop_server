77ccf5f7231814424f909f5b62de828a39845eb7
Roman Sidorov <vf@python.ru>
2019-07-15 22:00:31 +0300

    'Авторизация'

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from webapp.forms import LoginForm
 from webapp.models import db, Products
 
 from webapp.shoko_product_parser import get_product_list
@@ -15,5 +15,11 @@ def create_app():
         title = 'Меню Fake Шоколадницы '
         Products_list = Products.query.all()
         return render_template('index.html', page_title=title, Products_list=Products_list)
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
 
     return app
